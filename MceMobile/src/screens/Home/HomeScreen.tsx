fccc4fbdd9671ff2eb2dff570fd006aeec23f4e9
jcieloch <kuba300597@gmail.com>
2020-04-15 15:31:15 +0200

    testing whiteboard session

--- MceMobile/src/screens/Home/HomeScreen.tsx
+++ MceMobile/src/screens/Home/HomeScreen.tsx
@@ -1,15 +1,26 @@
+import 'react-native-get-random-values'
 import React from 'react'
 import { View, Text, Button } from 'react-native'
+import { WebView } from 'react-native-webview'
+
 
 class HomeScreen extends React.Component<{}, {}> {
 
     render() {
         return (
-            <View>
+            <View style={{flex:1}}>
                 <Text>Hello from home screen</Text>
                 <Text>Zobacz aktualne sesje</Text>
                 <Text>dołącz do nowej sesji</Text>
                 <Text>Zobacz zakończone sesje</Text>
+                <WebView 
+                 source={{ uri: 'http://10.0.2.2:8080/boards/Zad1' }}
+                 javaScriptEnabled={true}
+                 domStorageEnabled={true}
+                 startInLoadingState={true}
+                 scalesPageToFit={true}
+
+                />
             </View>
         )
     }
